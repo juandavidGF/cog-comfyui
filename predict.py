47564bc82e6da8572d9632ae90d21e01def9a133
juandavidGF <davad701@gmail.com>
2024-08-14 16:46:29 -0500

    x

--- predict.py
+++ predict.py
@@ -36,7 +36,9 @@ class Predictor(BasePredictor):
             workflow = json.loads(file.read())
         self.comfyUI.handle_weights(
             workflow,
-            weights_to_download=[],
+            weights_to_download=[
+                "control_sd15_depth.pth"
+            ],
         )
 
     def filename_with_extension(self, input_file, prefix):
